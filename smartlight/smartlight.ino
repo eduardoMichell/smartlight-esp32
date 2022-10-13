9f4e3491019c0c73d55596a0a713aeaad774b88b
eduardoMichell <eduardomichelgor@hotmail.com>
2022-10-13 11:49:11 -0300

    add button press to turn on and off the sensors

--- smartlight/smartlight.ino
+++ smartlight/smartlight.ino
@@ -1,12 +1,15 @@
 //PINS
-const int PIN_TO_SENSOR = 26;
-const int PIN_TO_LED = 27;
+const int PIN_TO_SENSOR = 27;
+const int PIN_TO_LED = 26;
 const int PIN_TO_LIGHT_SENSOR = 25;
-
+const int PIN_TO_BUTTON = 18;
 // STATES
 int pinStateCurrent   = LOW;  // current state of pin
 int pinStatePrevious  = LOW;  // previous state of pin
 
+int lastButtonState = HIGH; // the previous state from the input pin
+int currentButtonState;     // the current reading from the input pin
+int buttonState = LOW;     // the current reading from the input pin
 
 const int freq = 9000;
 const int ledChannel = 0;
@@ -16,52 +19,45 @@ void setup() {
   Serial.begin(9600);
   pinMode(PIN_TO_LED, OUTPUT);
   pinMode(PIN_TO_SENSOR, INPUT);
-
-
+  pinMode(PIN_TO_BUTTON, INPUT_PULLUP);
   ledcSetup(ledChannel, freq, resolution);
   ledcAttachPin(PIN_TO_LED, ledChannel);
 }
 
 void loop() {
-  pinStatePrevious = pinStateCurrent;
-  pinStateCurrent = digitalRead(PIN_TO_SENSOR);
-
-  if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {
-    Serial.println("Motion detected!");
-    digitalWrite(PIN_TO_LED, HIGH);
-    calculateBright(1);
+  currentButtonState = digitalRead(PIN_TO_BUTTON);
+  if (lastButtonState == LOW && currentButtonState == HIGH) {
+    buttonState = !buttonState;
+    Serial.print("Button Pressed: ");
+    Serial.println(buttonState);
   }
-  else
-  if (pinStatePrevious == HIGH && pinStateCurrent == LOW) {
-    Serial.println("Motion stopped!");
-    digitalWrite(PIN_TO_LED, LOW);
+
+  lastButtonState = currentButtonState;
+  if (buttonState == HIGH) {
+    pinStatePrevious = pinStateCurrent;
+    pinStateCurrent = digitalRead(PIN_TO_SENSOR);
+    if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {
+      Serial.println("Motion detected!");
+      calculateBright(1);
+    }
+    else if (pinStatePrevious == HIGH && pinStateCurrent == LOW) {
+      Serial.println("Motion stopped!");
+      calculateBright(0);
+    }
+  } else {
     calculateBright(0);
-  }  
+  }
+  delay(100);
 }
 
 
 void calculateBright(int state) {
   if (state == 1) {
-    // (value between 0 and 4095)
     int analogValue = analogRead(PIN_TO_LIGHT_SENSOR);
+    analogValue = map(analogValue, 0, 4095, 3000, 0);
+    ledcWrite(ledChannel, analogValue);
     Serial.print("Analog Value = ");
-    Serial.print(analogValue);
-    if (analogValue < 40) {
-      Serial.println(" => Dark");
-      ledcWrite(ledChannel, 255);
-    } else if (analogValue < 800) {
-      Serial.println(" => Dim");
-      ledcWrite(ledChannel, 200);
-    } else if (analogValue < 2000) {
-      Serial.println(" => Light");
-      ledcWrite(ledChannel, 150);
-    } else if (analogValue < 3200) {
-      Serial.println(" => Bright");
-      ledcWrite(ledChannel, 100);
-    } else {
-      Serial.println(" => Very bright");
-      ledcWrite(ledChannel, 0);
-    }
+    Serial.println(analogValue);
   } else {
     ledcWrite(ledChannel, 0);
   }
